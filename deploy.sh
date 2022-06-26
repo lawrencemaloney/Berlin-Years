aa217db4d67701dd74ab049b74309bcc5ba99a3b
Lawrence Maloney <lawrence.maloney@att.net>
2022-06-26 16:45:39 -0500

    Fix deploy  again

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,5 @@ npm run build
 
 echo "Deploying files to server..."
 scp -r build/* larrymm@173.230.133.229:/var/www/maloney-family-server/
-# scp -r build/* larrymm@173.230.133.229:/var/www/maloney-server/
 
 echo "Done! Hopefully"
